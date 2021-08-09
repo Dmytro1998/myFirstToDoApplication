093dff60e8a005099eb715fa26d51216367affb9
Dmytro <evdokimov041998@gmail.com>
2021-08-09 13:58:14 +0300

    scss + new styles

--- main.js
+++ main.js
@@ -13,43 +13,69 @@ function Task(description){
 }
 function createList(task, index){
     return `
-    <div class="item"${task.done ? 'checked' : ' '}>
-    <div class = "description">${task.description}</div>
-    <div class = "buttons">
+    <div class="item${task.done ? 'checked' : ' '}">
+       <div class ="checkboxAndDescription">
       <input onclick="completedTask(${index})" class="completed" type="checkbox"${task.done? 'checked':' '}>
-      <button class="delete">Delete</button>
-    </div>
+      <div class = "description">${task.description}</div>
+      </div>
+      <button class="delete"onclick ="deleteTask(${index})">Delete</button>
+    
   </div>
   `
 }
 
+function filterTask(){
+    let activeTasks = tasks.length && tasks.filter(item => item.done ==false);
+    let completedTask = tasks.length && tasks.filter(item => item.done ==true);
+ tasks = [...activeTasks, ...completedTask]
+}
+let todoItemElement =[]
+
 function fillList(){
     listOfTasks.innerHTML = "";
     if(tasks.length > 0 ){
+        filterTask()
         tasks.forEach((element,index) => {
             listOfTasks.innerHTML += createList(element, index)
             
-        })
-    }
+        });
+    }todoItemElement =document.querySelectorAll(".item")
 }
 fillList();
 function addToLocal(){
     localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
-// function completedTask(index){
+function completedTask(index){
    
    
-//     tasks[index].done =!tasks[index].done;
-//     if( tasks[index].done)
+    tasks[index].done =!tasks[index].done;
+    if( tasks[index].done){
+        todoItemElement[index].classList.add('checked')
 
-
+        
+}
+    else{
+        todoItemElement[index].classList.remove('checked')
+    }
+    addToLocal();
+    fillList();
+}
 
 addBtn.addEventListener("click",() =>{
+    if(inputField.value != ''){
     tasks.push(new Task(inputField.value))
     addToLocal();
     fillList();
     inputField.value = '';
 
 
-})
+}})
+
+
+
+function deleteTask(index){
+    tasks.splice(index,1)
+    addToLocal()
+    fillList()
+}
